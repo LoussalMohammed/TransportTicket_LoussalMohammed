ac51832cabcc7fd92d08751eaa5d139b6c495b8e
loussalmohammed <hplaptopsimo@gmail.com>
2024-09-02 14:43:52 +0100

    add scripts to handle database connection

---
+++ src/app/tools/DataFile.java
@@ -0,0 +1,22 @@
+package app.tools;
+
+public class DataFile {
+    public static void main(String[] args) {
+
+    }
+    public DataFile() {
+
+    }
+
+    public static String url() {
+        return "jdbc:postgresql://mohammed:5432/ecomove";
+    }
+
+    public static String user() {
+        return "postgres";
+    }
+
+    public static String password() {
+        return "superman";
+    }
+}
