4fbc0331d656bd11a7992445e51a3fa11969193a
loussalmohammed <hplaptopsimo@gmail.com>
2024-09-03 23:36:26 +0100

    rest entities Controller files

---
+++ src/org/app/Controllers/TicketController.java
@@ -0,0 +1,26 @@
+package org.app.Controllers;
+
+import org.app.Services.Admin.TicketServices;
+import org.views.admin.ticket.TicketView;
+/*
+public class TicketController {
+    /*private TicketServices ticketService;
+    private TicketView ticketView;
+
+    public TicketController() {
+        this.ticketService = new TicketService();
+        this.ticketView = new TicketView();
+    }
+
+    public void createTicket() {
+        // Logic to create a ticket
+        ticketService.createTicket();
+        ticketView.displayCreateSuccess();
+    }
+
+    public void viewTickets() {
+        // Logic to view tickets
+        List<Ticket> tickets = ticketService.getAllTickets();
+        ticketView.displayTickets(tickets);
+    }
+}*/
