2807b1fc810c81e72ef13c37146468da292d0ac8
loussalmohammed <hplaptopsimo@gmail.com>
2024-09-03 23:39:45 +0100

    Password Util to hash and check hashed password

---
+++ src/org/app/Models/Helpers/PasswordUtil.java
@@ -0,0 +1,13 @@
+package org.app.Models.Helpers;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String password, String hashedPassword) {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+}
