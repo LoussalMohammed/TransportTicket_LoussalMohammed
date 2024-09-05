6934525e6bbb4db87be0b967104eb8ace18c0f68
loussalmohammed <hplaptopsimo@gmail.com>
2024-09-05 06:22:05 +0100

    Add Helpers To Hash Password Validate Inputs

---
+++ src/org/app/Models/Helpers/InputValidator.java
@@ -0,0 +1,31 @@
+package org.app.Models.Helpers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+    public static boolean validateName(String input) {
+        String pattern = ".*[A-Za-z]{3,}.*";
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(input);
+        return matcher.find();
+    }
+    public static boolean validatePassword(String input) {
+        String pattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";;
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(input);
+        return matcher.matches();
+    }
+    public static boolean validateEmail(String input) {
+        String pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(input);
+        return matcher.find();
+    }
+    public static boolean validatePhoneNumber(String input) {
+        String pattern = ".*\\d{9,}.*";
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(input);
+        return matcher.find();
+    }
+}
